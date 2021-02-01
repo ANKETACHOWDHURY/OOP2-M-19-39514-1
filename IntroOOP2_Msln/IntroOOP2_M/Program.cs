941eefb0348863a5682317bda471b38689f0089b
Anketa <chowdhuryanketa@gmail.com>
2021-02-01 10:55:27 +0600

    My first task

---
+++ IntroOOP2_Msln/IntroOOP2_M/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IntroOOP2_M
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5, y = 9;
+            
+            Console.WriteLine("Addition of x and y is {0}", x+y);
+            Console.WriteLine("Subtraction of x and y is {0}", x-y);
+            Console.WriteLine("Multiplication of x and y is {0}", x*y);
+            Console.WriteLine("Divition of x and y is {0}", x / y);
+            Console.WriteLine("The number of the w is:");
+            int w = int.Parse(Console.ReadLine());         
+        }
+    }
+}
